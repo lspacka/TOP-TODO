db593248f9c38820de82cdc481821eedaa560532
julian <jdrc@protonmail.com>
2024-04-10 20:21:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## [The Odin Project - TODO List](https://www.theodinproject.com/lessons/node-path-javascript-todo-list)
+
+### Built With: 
+
+- HTML
+- CSS 
+- JavaScript
+
+Font - [Geist](https://vercel.com/font)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      // inject: 'head',
+      scriptLoading: 'defer',
+    })
+  ]
+}
