3c0dbbd48ff60b1d53a0f55bb72cc888b7fe81ea
julian <jdrc@protonmail.com>
2024-05-12 16:24:21 -0500

    add project display

--- src/DOMmod.js
+++ src/DOMmod.js
@@ -3,11 +3,36 @@ export function updateProject(div, pros) {
     let project = pros[pros.length-1]
 
     button.textContent = project.name
-    button.classList.add('user-project-button')
+    button.setAttribute("class", "project-button user-project-button")
+    // button.classList.add('user-project-button')
     div.appendChild(button)
 }
 
-export function showProject(div, pro) {
+export function showProject(heading, list, pro) {
+    list.innerHTML = ''
+    let tasks = pro.tasks
+    heading.textContent = pro.name
+
+    if (tasks.length > 0) {
+        tasks.forEach((task, index) => {
+            let li = document.createElement('li')
+            let button = document.createElement('button')
+
+            button.classList.add('.detail-button')
+            button.textContent = 'Detail'
+            li.setAttribute('key', index)
+            li.textContent = task.title
+            li.appendChild(button)
+            list.appendChild(li)
+        })  
+    }
+}
+
+export function deleteTask() {
+    //
+}
+
+export function TaskDetail(task) {
     //
 }
 
@@ -28,8 +53,4 @@ export function ModalHandler(newPro, cancelPro, newTask, cancelTask, proModal, t
         taskModal.close()
         console.log('this guy fucks')
     })
-}
-
-export function TaskDetail(task) {
-    //
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 //  TODO:
-//   - revise var names
-//   - ModalHandler
+//  + showProject()
+//    + update heading
+//    + forEach task => <li>task.title<button class="task-detail"><li>
+//  - delete task
+//  - task detail modal. div.textContent = task.desc + close button
 
 import './style.css'
-import { ModalHandler, updateProject } from './DOMmod.js'
+import { ModalHandler, updateProject, showProject } from './DOMmod.js'
 
-const project_btns = document.querySelectorAll('.project-button')
+const pro_buttons = document.querySelectorAll('.project-button')
+const def_pro_btns = document.querySelectorAll('.default-project-button')
 const user_pro_btns = document.querySelectorAll('.user-project-button')
 const user_section = document.querySelector('.user-projects')
 const new_project = document.querySelector('.add-project')
@@ -13,6 +17,7 @@ const add_project = document.getElementById('add-project')
 const cancel_project = document.getElementById('cancel-project')
 const project_modal = document.querySelector('.project-modal')
 const project_name = document.querySelector('.project-name')
+const pro_heading = document.querySelector('.project-heading')
 // const modal_heading = document.querySelector('.modal-heading')
 const new_task = document.querySelector('.new-task-btn')
 const task_modal = document.querySelector('.task-modal')
@@ -20,13 +25,15 @@ const task_title = document.querySelector('.task-title')
 const task_desc = document.querySelector('.task-desc')
 const add_task = document.getElementById('add-task')
 const cancel_task = document.getElementById('cancel-task')
+const pro_display = document.querySelector('.project-display')
+const tasks_list = document.querySelector('.tasks-list')
 
 class Task {
   constructor(title, desc, date, pro) {
     this.title = title
     this.desc = desc
     this.date = date
-    this.pro = pro
+    this.pro = pro  // ?
     this.important = false
   }
 }
@@ -63,6 +70,7 @@ let important = {
 let user_pros = []
 let current_pro = default_pro
 let all_pros = [default_pro, today, this_week, important]
+showProject(pro_heading, tasks_list, current_pro)
 
 //  clusterfuck procedure for adding event listeners to dynamically added buttons (user projects)
 //  the event listener basically updates the current_pro variable
@@ -73,6 +81,7 @@ function handleButtonClick(e) {
       if (btn_text == pro.name) current_pro = pro;
   });
   console.log("Current project:", current_pro);
+  showProject(pro_heading, tasks_list, current_pro)
 }
 
 function addButtonClickListener(button) {
@@ -91,6 +100,7 @@ function addNewProject() {
   all_pros.push(project);
   updateProject(user_section, user_pros);
   project_modal.close()
+  showProject(pro_heading, tasks_list, current_pro)
 
   let newButton = document.querySelector('.user-project-button:last-child')
   if (newButton) {
@@ -102,26 +112,16 @@ function addNewProject() {
 add_project.addEventListener('click', addNewProject);
 
 //  Event listeners for the default projects
-project_btns.forEach(btn => {
+def_pro_btns.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    // let btn_text = e.target.textContent.toLowerCase().split(' ').join('_') 
     let btn_text = e.target.textContent.toLowerCase()
     all_pros.forEach(pro => {
       if (btn_text == pro.name) current_pro = pro
     })
     console.log("Current project:", current_pro)
+    showProject(pro_heading, tasks_list, current_pro)
   })
 })
-
-// add_project.addEventListener('click', () => {
-//   let name = project_name.value
-//   let project = new Project(name)
-
-//   user_pros.push(project)
-//   all_pros.push(project)
-//   showProject(user_section, user_pros, project_btns)
-//   // console.log(all_pros)
-// })
 
 ModalHandler(
   new_project,
@@ -132,23 +132,6 @@ ModalHandler(
   task_modal
 )
 
-// new_project.addEventListener('click', () => {
-//   project_modal.show()
-// })
-
-// cancel_project.addEventListener('click', () => {
-//   project_modal.close()
-// })
-
-// new_task.addEventListener('click', () => {
-//   task_info.show()
-// })
-
-// cancel_task.addEventListener('click', () => {
-//   task_info.close()
-//   console.log('this guy fucks')
-// })
-
 add_task.addEventListener('click', () => {
   let title = task_title.value
   let desc = task_desc.value
@@ -156,5 +139,6 @@ add_task.addEventListener('click', () => {
 
   current_pro.tasks.push(task)
   task_modal.close()
+  showProject(pro_heading, tasks_list, current_pro)
   console.log(current_pro)
 })
